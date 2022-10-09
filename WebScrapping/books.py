d295fe73b92727beb9ce5565dd3ec7833b243545
MrFahadRajput <94325634+Mr-Fahad-Rajput@users.noreply.github.com>
2022-10-09 20:07:43 +0500

    Assignment 1 Complete

--- WebScrapping/books.py
+++ WebScrapping/books.py
@@ -1,6 +1,8 @@
+from array import array
 import requests
 from bs4 import BeautifulSoup
 import re
+import pandas as pd
 
 site = requests.get('https://entertainment.time.com/2005/10/16/all-time-100-novels/slide/all/')
 soup = BeautifulSoup(site.content, 'html.parser')
@@ -16,21 +18,20 @@ links.pop(0)
 links.pop()
 links.pop() #Links Finalized
 
-
-linkString = ",".join(str(x) for x in links)
-
-
-
+#usableData Extraction
+linkString = ",".join(str(x) for x in links)# Have to convert Each Result to a string to perform regex Searches
 linksCleaned = re.sub(',' , '   ', linkString)
 linksCleaned = re.sub('/' , '_', linksCleaned)
 linksCleaned = re.sub('-' , '_', linksCleaned)
-
-
 usableData = re.findall('_slide_\w+', linksCleaned)
-usableData = ",".join(str(x) for x in usableData) # Have to convert Each Result to a string to perform regex Searches
+usableData = ",".join(str(x) for x in usableData) 
 
 #Date Extraction
-dates = re.findall('\d+', usableData) # All Dates Finalized
+dates = re.findall('_\d+_by', usableData) 
+dates = ",".join(str(x) for x in dates) 
+dates = re.sub('_by' , ' ', dates)
+dates = re.sub('_' , ' ', dates)
+dates = re.split(',' ,dates) # All Dates Finalized
 
 #Extracting Author Names
 authorNames = re.findall('_by_\w+', usableData)
@@ -53,9 +54,15 @@ bookNames = re.sub('\s+' , ' ', bookNames)
 bookNames = re.split(',' ,bookNames)
 bookNames = [bookName.title() for bookName in bookNames] # Book Names Finalized
 
+#Using Pandas for Visualization
+finalTable = pd.DataFrame({
+    'Book Title' : bookNames,
+    'Date Published' : dates,
+    'Author' : authorNames,
+    'URLS to the Book Review' : links
+})
 
 
-
-print(bookNames, authorNames, dates)
+print(finalTable)
 
 
